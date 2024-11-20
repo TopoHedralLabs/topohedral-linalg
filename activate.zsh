f361270ad70b59d9d29595a88bf9da478e4f89f2
John Ferguson <JAFerguson952@gmail.com>
2024-11-20 23:36:40 +0000

    no longer documenting private items

--- activate.zsh
+++ activate.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
+export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html"
 export TOPO_LOG=all=trace
