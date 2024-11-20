5a4a1ba52bb9b85d5bfbce14076ac8d54e2fba53
John Ferguson <JAFerguson952@gmail.com>
2024-11-20 23:35:12 +0000

    added docs

--- src/matrix/smatrix.rs
+++ src/matrix/smatrix.rs
@@ -19,11 +19,30 @@ use rand::distributions::{Distribution, Uniform};
 
 
 //{{{ struct: SMatrix
+/// A fixed-size $N \times M$ matrix type that stores its elements in a static, contiguous array.
+/// 
+/// The `SMatrix` struct represents a 2D matrix with a fixed size, where the dimensions
+/// are specified as generic parameters `N` and `M`. The elements of the matrix are
+/// stored in a contiguous array, which allows for efficient access and manipulation.
+/// 
+/// The matrix is stored in column-major order, which means a matrix is stored column by column
+/// in memory. So, for example, the matrix:
+/// ```ignore
+/// 1 2 3
+/// 4 5 6
+/// 7 8 9
+/// ```
+/// will be stored as:
+/// ```ignore
+/// 1 4 7 2 5 9 3 6 9
+/// ```
+/// In memory 
 #[derive(Clone)]
 pub struct SMatrix<T, const N: usize, const M: usize> 
     where [(); N*M]:, 
     T: Field + Default + Copy + fmt::Display, 
 {
+    /// The data of the matrix, stored in column-major order.
     pub(crate) data: [T; N*M],
 }
 //}}}
