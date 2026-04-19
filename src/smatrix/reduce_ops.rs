175b42560ea82e594f59e9a2a85444b4ad31e2e0
John Ferguson <JAFerguson95@gmail.com>
2026-04-19 22:16:50 +0100

    implemented reduce ops and transform ops for matrix views (#36)

--- src/smatrix/reduce_ops.rs
+++ src/smatrix/reduce_ops.rs
@@ -4,7 +4,10 @@
 //--------------------------------------------------------------------------------------------------
 
 //{{{ crate imports
-use super::SMatrix;
+use super::{
+    subviews::{MatrixView, MatrixViewMut},
+    SMatrix,
+};
 use crate::common::{tuple_index, Field};
 use crate::ReduceOps;
 //}}}
@@ -50,6 +53,100 @@ where
         for (linear_index, &value) in self.data.iter().enumerate()
         {
             acc = f(acc, tuple_index(linear_index, N), value);
+        }
+        acc
+    }
+}
+
+impl<'a, T, const N: usize, const M: usize> ReduceOps for MatrixView<'a, T, N, M>
+where
+    [(); N * M]:,
+    T: Field + Copy,
+{
+    type Item = T;
+    type Index = (usize, usize);
+
+    fn fold<B, F>(
+        &self,
+        init: B,
+        mut f: F,
+    ) -> B
+    where
+        F: FnMut(B, Self::Item) -> B,
+    {
+        let mut acc = init;
+        for col in 0..self.ncols
+        {
+            for row in 0..self.nrows
+            {
+                acc = f(acc, self[(row, col)]);
+            }
+        }
+        acc
+    }
+
+    fn fold_indexed<B, F>(
+        &self,
+        init: B,
+        mut f: F,
+    ) -> B
+    where
+        F: FnMut(B, Self::Index, Self::Item) -> B,
+    {
+        let mut acc = init;
+        for col in 0..self.ncols
+        {
+            for row in 0..self.nrows
+            {
+                acc = f(acc, (row, col), self[(row, col)]);
+            }
+        }
+        acc
+    }
+}
+
+impl<'a, T, const N: usize, const M: usize> ReduceOps for MatrixViewMut<'a, T, N, M>
+where
+    [(); N * M]:,
+    T: Field + Copy,
+{
+    type Item = T;
+    type Index = (usize, usize);
+
+    fn fold<B, F>(
+        &self,
+        init: B,
+        mut f: F,
+    ) -> B
+    where
+        F: FnMut(B, Self::Item) -> B,
+    {
+        let mut acc = init;
+        for col in 0..self.ncols
+        {
+            for row in 0..self.nrows
+            {
+                acc = f(acc, self[(row, col)]);
+            }
+        }
+        acc
+    }
+
+    fn fold_indexed<B, F>(
+        &self,
+        init: B,
+        mut f: F,
+    ) -> B
+    where
+        F: FnMut(B, Self::Index, Self::Item) -> B,
+    {
+        let mut acc = init;
+        for col in 0..self.ncols
+        {
+            for row in 0..self.nrows
+            {
+                acc = f(acc, (row, col), self[(row, col)]);
+            }
         }
         acc
     }
--- tests/reduce_ops.rs
+++ tests/reduce_ops.rs
@@ -6,6 +6,39 @@ mod dmatrix_tests
     use topohedral_linalg::dmatrix::*;
     use topohedral_linalg::ReduceOps;
 
+    fn assert_rectangular_view_reductions<R>(view: &R)
+    where
+        R: ReduceOps<Item = i32, Index = (usize, usize)>,
+    {
+        let traversal = view.fold(Vec::new(), |mut acc, value| {
+            acc.push(value);
+            acc
+        });
+        let indexed_traversal = view.fold_indexed(Vec::new(), |mut acc, index, value| {
+            acc.push((index, value));
+            acc
+        });
+
+        assert_eq!(traversal, vec![6, 10, 14, 7, 11, 15]);
+        assert_eq!(
+            indexed_traversal,
+            vec![
+                ((0, 0), 6),
+                ((1, 0), 10),
+                ((2, 0), 14),
+                ((0, 1), 7),
+                ((1, 1), 11),
+                ((2, 1), 15),
+            ]
+        );
+        assert_eq!(view.sum(), 63);
+        assert_eq!(view.product(), 970200);
+        assert_eq!(view.min(), Some(6));
+        assert_eq!(view.max(), Some(15));
+        assert_eq!(view.argmin(), Some(((0, 0), 6)));
+        assert_eq!(view.argmax(), Some(((2, 1), 15)));
+    }
+
     #[test]
     fn test_fold_and_fold_indexed()
     {
@@ -63,12 +96,71 @@ mod dmatrix_tests
         assert_eq!(a.argmin(), None);
         assert_eq!(a.argmax(), None);
     }
+
+    #[test]
+    fn test_immutable_subview_reductions_use_column_major_order_and_local_indices()
+    {
+        let matrix = DMatrix::<i32>::from_row_slice(
+            &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            4,
+            4,
+        );
+        let view = matrix.subview(1, 3, 1, 2);
+
+        assert_rectangular_view_reductions(&view);
+    }
+
+    #[test]
+    fn test_mutable_subview_reductions_use_column_major_order_and_local_indices()
+    {
+        let mut matrix = DMatrix::<i32>::from_row_slice(
+            &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+            4,
+            4,
+        );
+        let view = matrix.subview_mut(1, 3, 1, 2);
+
+        assert_rectangular_view_reductions(&view);
+    }
 }
 
 mod smatrix_tests
 {
     use topohedral_linalg::smatrix::*;
     use topohedral_linalg::ReduceOps;
+
+    fn assert_rectangular_view_reductions<R>(view: &R)
+    where
+        R: ReduceOps<Item = i32, Index = (usize, usize)>,
+    {
+        let traversal = view.fold(Vec::new(), |mut acc, value| {
+            acc.push(value);
+            acc
+        });
+        let indexed_traversal = view.fold_indexed(Vec::new(), |mut acc, index, value| {
+            acc.push((index, value));
+            acc
+        });
+
+        assert_eq!(traversal, vec![6, 10, 14, 7, 11, 15]);
+        assert_eq!(
+            indexed_traversal,
+            vec![
+                ((0, 0), 6),
+                ((1, 0), 10),
+                ((2, 0), 14),
+                ((0, 1), 7),
+                ((1, 1), 11),
+                ((2, 1), 15),
+            ]
+        );
+        assert_eq!(view.sum(), 63);
+        assert_eq!(view.product(), 970200);
+        assert_eq!(view.min(), Some(6));
+        assert_eq!(view.max(), Some(15));
+        assert_eq!(view.argmin(), Some(((0, 0), 6)));
+        assert_eq!(view.argmax(), Some(((2, 1), 15)));
+    }
 
     #[test]
     fn test_fold_and_fold_indexed()
@@ -126,5 +218,27 @@ mod smatrix_tests
         assert_eq!(a.max(), None);
         assert_eq!(a.argmin(), None);
         assert_eq!(a.argmax(), None);
+    }
+
+    #[test]
+    fn test_immutable_subview_reductions_use_column_major_order_and_local_indices()
+    {
+        let matrix = SMatrix::<i32, 4, 4>::from_row_slice(&[
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        ]);
+        let view = matrix.subview(1, 3, 1, 2);
+
+        assert_rectangular_view_reductions(&view);
+    }
+
+    #[test]
+    fn test_mutable_subview_reductions_use_column_major_order_and_local_indices()
+    {
+        let mut matrix = SMatrix::<i32, 4, 4>::from_row_slice(&[
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+        ]);
+        let view = matrix.subview_mut(1, 3, 1, 2);
+
+        assert_rectangular_view_reductions(&view);
     }
 }
