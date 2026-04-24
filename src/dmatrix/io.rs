de1b66cadc79c9927771f9843d18adc31821f9a5
John Ferguson <JAFerguson952@gmail.com>
2026-04-24 21:17:45 +0100

    fixed printing format and bump version

--- src/dmatrix/io.rs
+++ src/dmatrix/io.rs
@@ -17,26 +17,19 @@ use std::fmt;
 //{{{ impl fmt::Display for SMatrix
 impl<T> fmt::Display for DMatrix<T>
 where
-    T: Field + Default + Copy + fmt::Display,
+    T: Field + Default + Copy + fmt::Display + fmt::LowerExp,
 {
     fn fmt(
         &self,
         f: &mut fmt::Formatter<'_>,
     ) -> fmt::Result
     {
-        let max_width = self
-            .data
-            .iter()
-            .map(|x| format!("{x}").len())
-            .max()
-            .unwrap_or(0);
-
         for i in 0..self.nrows
         {
             write!(f, "|")?;
             for j in 0..self.ncols
             {
-                write!(f, " {:>width$}", self[(i, j)], width = max_width)?;
+                write!(f, " {:1.4e} ", self[(i, j)],)?;
             }
             writeln!(f, " |")?;
         }
