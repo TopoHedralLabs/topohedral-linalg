4f7d4b0c2c743fa4c705cfe415f4c15c31d143f3
John Ferguson <JAFerguson952@gmail.com>
2026-05-02 10:38:56 +0100

    cleanup

--- activate.zsh
+++ activate.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
+export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/rustdoc-html/custom-header.html --document-private-items"
 export TOPO_LOG=all=trace
